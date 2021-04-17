cce69044e9c1ab1f6c7453300e93b93769a5b4d2
QuantumCalzone <QuantumCalzone@gmail.com>
2021-04-17 12:14:56 -0700

    Revert "Updated unity version"
    This reverts commit 0cd0eb626f20c6898a229115beb1340f2ca67e70.

---
+++ Scripts/LayoutElementExtendedValue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using UnityEngine;
+
+namespace QuantumCalzone
+{
+    [Serializable]
+    public class LayoutElementExtendedValue
+    {
+        public enum ReferenceTypes { None, Width, Height }
+
+        public bool Enabled = false;
+        public ReferenceTypes ReferenceType = ReferenceTypes.None;
+        public RectTransform Reference = null;
+        public float ReferenceDelta = 0;
+        public float TargetValue = 0;
+
+        public void ProcessTargetValue(UnityEngine.Object context)
+        {
+            if (!Reference && ReferenceType != ReferenceTypes.None)
+            {
+                Debug.Log("This needs a reference to process your target Layout values", context);
+                return;
+            }
+
+            switch (ReferenceType)
+            {
+                case ReferenceTypes.None:
+                    break;
+                case ReferenceTypes.Width:
+                    TargetValue = Reference.rect.width;
+                    break;
+                case ReferenceTypes.Height:
+                    TargetValue = Reference.rect.height;
+                    break;
+                default:
+                    Debug.LogError(string.Format("No support for a referenceType of: {0}", ReferenceType), context);
+                    break;
+            }
+
+            TargetValue += ReferenceDelta;
+        }
+    }
+}
